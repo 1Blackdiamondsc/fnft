fadfe2611ae30866b294a73ed818cfdcf140bf55
BlackDiamond SC Inc [BDSCI TOKEN] <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-10-03 23:39:36 -0400

    Update and rename TestKitty.sol to BabyApes.sol

--- truffle/contracts/BabyApes.sol
+++ truffle/contracts/BabyApes.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 
 contract TheBoredApesFNFT is ERC721Token {
-  constructor() ERC721Token("TheBoredApesFNFT", "TBAF") {}
+  constructor() ERC721Token("BabyApes", "BBA") {}
   function mint(uint tid) public { _mint(msg.sender, tid); }
 }
